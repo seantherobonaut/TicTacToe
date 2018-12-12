8d027603fa23883ebd06896c2f13531931db0e95
Sean Leapley <seantherobonaut@gmail.com>
2018-12-11 16:11:13 -0800

    Save all progress

--- game.js
+++ game.js
@@ -1,14 +1,21 @@
-const Board = function(sizeX=0, sizeY=0)
+const Board = function(id="none", sizeX=0, sizeY=0)
 {
     checkInstance(this, Board);
-    //TODO write checks for constructor ints
+    //TODO write checks for constructor properties
 
-    let board = new Array();
+    //Constructor
+    this.Board = function()
+    {
+        //console.log("Board was created!");
+        let target = document.getElementById(id);
+        console.log("I have been created!");
+    };
 
     this.getState = function()
     {
 
     };
 
-    console.log("Board was created!");
+    //run constructor
+    this.Board();
 };
