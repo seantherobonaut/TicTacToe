03cdf008fc49b3602469e598782237222659f4f8
Sean Leapley <seantherobonaut@gmail.com>
2019-02-05 16:16:17 -0800

    Save all progress

--- Board.js
+++ Board.js
@@ -3,16 +3,16 @@ class Board
     //Constructor
     constructor(targetID="none", sizeX=0, sizeY=0)
     {
-        this._board = new Array;
         this._sizeX = sizeX;
         this._sizeY = sizeY;
+        this._board = new Array;
 
         //console.log("Board was created!");
-        let target = document.getElementById(targetID);
-        if(target)
+        this._target = document.getElementById(targetID);
+        if(this._target)
             console.log("I have been created!");
         else
-            console.log("Unable to find element with that id.");
+            console.log("Unable to find element with that id."); //throw new error
 
         //Fill the board using sizeX and sizeY
         for(let i=0; i<this._sizeY; i++)
@@ -23,18 +23,40 @@ class Board
         }        
     }
 
-    //TODO Make sure when you return a copy of this that it is NOT a reference!
-    getState()
-    {
-        return this._board;
+    render()
+    {        
+        let result = "";
+        //<div><span>O</span><span>X</span><span>X</span></div>
+        for(let i=0; i<sizeY; i++)
+        {
+            result.concat("<div>");
+            for(let ii=0; ii<sizeX; ii++)
+            {
+                result.concat("<span>");
+                result.concat(this._board[ii][i]);
+                result.concat("</span>");
+            }
+            result.concat("</div>");
+        }
+
+        return result;
+        
     }
 
-    //Checking for valid entries will be done by an external function. Return 0 if spot is taken, return -1 if out of bounds
+    //Return array by value rather than reference
+    getState()
+    {
+        return this._board.slice(0);
+    }
+
+    //Checking for valid entries will be done by an external function. Return 0 if spot is taken, return -1 if out of bounds, true otherwise
     setMove(playerID, x, y)
     {
         //TODO actually do validation: playerID and x/y must be whole numbers above 0 and within board boundaries
         //TODO maybe convert input to reverse "y" and subtract by offset? or willl that just confuse the AI? 
         //TODO remember to add "1" to input values
+        x--;
+        y--;
 
         if(x >= 0 && x < this._sizeX && y >= 0 && y < this._sizeX)
         {
